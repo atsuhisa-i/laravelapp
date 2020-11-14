67efeac96b39b3f043c6ecbfdf617f44b907b798
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-11-14 13:30:20 +0900

    ビューの勉強1

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('hello', function () {
+    return view('hello.index');
 });
-
-Route::get('hello', 'App\Http\Controllers\HelloController@index');
