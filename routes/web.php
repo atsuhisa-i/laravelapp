3d5989f62964caaffa4eb66bc07bc08114edb491
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-11-18 21:37:46 +0900

    ミドルウェアの勉強3

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,5 @@ use App\Http\Middleware\HelloMiddleware;
 |
 */
 
-Route::get('hello', 'App\Http\Controllers\HelloController@index')
-    ->middleware(HelloMiddleware::class);
+Route::get('hello', 'App\Http\Controllers\HelloController@index');
 Route::post('hello', 'App\Http\Controllers\HelloController@post');
