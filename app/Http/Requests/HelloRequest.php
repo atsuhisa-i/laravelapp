f25b6f34e09c8b0fbf574127ec56af96203c3dbd
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-11-22 23:15:03 +0900

    バリデーションの勉強6

--- app/Http/Requests/HelloRequest.php
+++ app/Http/Requests/HelloRequest.php
@@ -34,4 +34,12 @@ class HelloRequest extends FormRequest
             'age' => 'numeric|between:0,150',
         ];
     }
+    public function messages()
+    {
+        return [
+            'name.required' => '名前は必ず入力して下さい。',
+            'mail.email' => 'メールアドレスが必要です。',
+            'age.numeric' => '年齢を整数で記入ください。',
+        ];
+    }
 }
