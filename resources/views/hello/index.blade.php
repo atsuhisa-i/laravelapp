694bbc7d35c54541ec6e529b30e7be278b7b75f0
atsuhisa-i <atsuhisa.1124@gmail.com>
2020-12-05 13:49:29 +0900

    ユーザー認証2

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -17,6 +17,12 @@
 @endsection
 
 @section('content')
+@if (Auth::check())
+<p>USER: {{$user->name . '(' . $user->email . ')'}}</p>
+@else
+<p>※ログインしていません。(<a href="/login">ログイン</a>|
+    <a href="/register">登録</a>)</p>
+@endif
   <table>
   <tr>
       <th><a href="/hello?sort=name">name</a></th>
@@ -31,7 +37,7 @@
     </tr>
   @endforeach
   </table>
-  {{ $items->appends(['sort' => $sort])->links('vendor.pagination.semantic-ui') }}
+  {{ $items->appends(['sort' => $sort])->links() }}
 @endsection
 
 @section('footer')
